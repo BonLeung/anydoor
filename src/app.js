bd4c0b08889bf5c775dc869edab9bf2cd7189733
BonLeung <654592397@qq.com>
2017-11-07 18:16:57 +0800

    commit

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 const http = require('http')
+const path = require('path')
 const conf = require('./config/defaultConfig')
+const route = require('./helper/route')
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader("Content-Type", "text/plain")
-    res.end("Hello World")
+    const filePath = path.join(conf.root, req.url)
+    route(req, res, filePath)
 })
 
 server.listen(conf.port, conf.hostname, () => {
